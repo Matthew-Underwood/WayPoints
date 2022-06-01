061175fa83d639dad85c490f71887362144949f8
farban6 <farban6@gmail.com>
2022-06-01 13:22:16 +0100

    Fixed a few bugs but bugs still remain. Need to add waypoints as child of node.

--- addons/waypoints/src/waypoints_factory.gd
+++ addons/waypoints/src/waypoints_factory.gd
@@ -2,7 +2,6 @@ class_name MUW_Waypoints_Factory
 
 var _pathing_factory : MUP_Pathing_Factory
 var _world : MUP_World
-var _waypoints_packed : PackedScene
 var _transformer_factory : MUW_Waypoint_Transformer_Factory
 
 func _init(
@@ -16,21 +15,22 @@ func _init(
 
 func create_2d() -> Node2D:
 	var tilemap_transformer = _transformer_factory.create_tilemap_transformer()
-	# TODO Need to seperate this tscn into 2d and 3d
-	var tilemap_waypoint = preload("res://addons/waypoints/scenes/tilemap_waypoint.tscn")
-	return _create_waypoints(tilemap_transformer, tilemap_waypoint)
+	var waypoints = preload("res://addons/waypoints/scenes/2d/waypoints.tscn")
+	var waypoint = preload("res://addons/waypoints/scenes/2d/waypoint.tscn")
+	return _create_waypoints(tilemap_transformer, waypoints, waypoint)
 
 
 func create_spatial() -> Spatial:
 	var mesh_transformer = _transformer_factory.create_mesh_transformer()
-	# TODO Need to seperate this tscn into 2d and 3d
-	var spatial_waypoint = preload("res://addons/waypoints/scenes/spatial_waypoint.tscn")
-	return _create_waypoints(mesh_transformer, spatial_waypoint)
+	var waypoints = preload("res://addons/waypoints/scenes/spatial/waypoints.tscn")
+	var waypoint = preload("res://addons/waypoints/scenes/spatial/waypoint.tscn")
+	return _create_waypoints(mesh_transformer, waypoints, waypoint)
 	
 
-func _create_waypoints(transformer, packed_waypoint : PackedScene):
+func _create_waypoints(transformer, packed_waypoints : PackedScene, packed_waypoint : PackedScene):
 	var pathing = _pathing_factory.create(_world)
-	var waypoints = _waypoints_packed.instance()
+	var waypoints = packed_waypoints.instance()
+	
 	waypoints.set_waypoint(packed_waypoint)
 	waypoints.set_world(_world)
 	waypoints.set_pathing(pathing)
