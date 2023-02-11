2251ee10b67dad1ab722d1e7e9b8719e0480505f
farban6 <farban6@gmail.com>
2023-02-11 19:33:23 +0000

    more testing

--- addons/waypoints/src/waypoint/spatial.gd
+++ addons/waypoints/src/waypoint/spatial.gd
@@ -26,21 +26,26 @@ func set_camera(camera : Camera):
 
 func set_path(path : Array) -> void:
 
-	path.pop_back()
-	if !_path.empty():
-		for point_id in range(1, len(_path)):
+	var line_dot
+	if !path.empty():
+		for point_id in range(1, len(path)):
 			var line = load("res://addons/waypoints/scenes/spatial/line.tscn")
 			line = line.instance()
-			var previous_point = path[point_id - 1]
 			var point = path[point_id]
-
-			var point = _transformer.transform(item)
+			var previous_point = path[point_id - 1]
+			var radians = _get_facing_angle(line.transform, previous_point, point)
+			line.transform = line.transform.rotated(Vector3(0, 1, 0), radians)
+			point = _transformer.transform(point)
 			line.transform.origin = point - transform.origin
 			add_child(line)
 
-func _get_direction(pos : Vector3, pos2 : Vector3):
-	var pos_unit = pos.normalized()
-	var direction = pos.direction_to(pos2)
-	direction.dot()
-	match direction:
-		0:
+func _get_facing_angle(transform : Transform, pos : Vector3, pos2 : Vector3):
+	var facing = transform.basis.z
+	var y_axis = transform.basis.y
+	var direction = pos - pos2
+	var rad = facing.signed_angle_to(direction, y_axis)
+	print(rad2deg(rad))
+	return rad
+	
+
+
--- test_dot.gd
+++ test_dot.gd
@@ -10,13 +10,19 @@ extends Spatial
 func _ready():
 	var line = $Line
 	var line2 = $Line2
-	var line_pos = line.transform.origin
-	var line_pos2 = line2.transform.origin
-	var line_unit = line_pos.normalized()
-	var direction = line_pos.direction_to(line_pos2)
-	var dot = direction.dot(line_unit)
-	print(direction)
-	print(dot)
+
+	#var line_facing = line.transform.basis.z
+	#var line_pos = line.transform.origin
+	#var line_pos2 = line2.transform.origin
+	#var facing = line_pos.direction_to(line_pos2)
+	#var dot = line_facing.dot(facing)
+	#print(dot)
+	var line_basis = line.transform.basis.z
+	var line_origin = line.transform.origin
+	var line_origin2 = line2.transform.origin
+	var direction = line_origin - line_origin2
+	var angle = line_basis.signed_angle_to (direction, line.transform.basis.y)
+	print(rad2deg(angle))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
