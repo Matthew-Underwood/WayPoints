c1bd961903b5b61d11a6b6254341d035bf789bc5
Matthew Underwood <farban6@gmail.com>
2023-05-20 14:07:33 +0100

    Remove testing

--- line-test.gd
+++
@@ -1,8 +0,0 @@
-extends MeshInstance
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var line_node = $Line
-	var line = MUW_Line_Factory.new()
-	line.create(line_node, [Vector3(0.5 ,0, 0.5), Vector3(1.5 ,0, 0.5), Vector3(2.5 ,0.2 , 0.5)])
