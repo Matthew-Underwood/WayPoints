28d566690b468a4c4b6306a75928b82ec0c86043
farban6 <farban6@gmail.com>
2024-02-18 12:39:37 +0000

    Very minor formatting and playing around with how the waypoint nodes will work.

--- addons/waypoints/src/waypoint/spatial.gd
+++ addons/waypoints/src/waypoint/spatial.gd
@@ -10,11 +10,14 @@ var _waypoint_data : MUW_Waypoint_Data
 
 func set_waypoint_data(waypoint_data : MUW_Waypoint_Data):
 	_waypoint_data = waypoint_data
+	_update_path()
 
 
-func update_path():
-	pass
-	#TODO how does this fit in with new system
+func _update_path():
+	if get_parent() == null:
+		yield(self, "ready")
+		transform.origin = path[len(path) - 1]
+		$Line.create_line(path, _points)
 
 
 #TODO remove this when sure its refactored
@@ -29,10 +32,3 @@ func get_world_position() -> Vector2:
 #TODO what are we doing with this?
 func set_id(id : String):
 	$Label3D.text = id
-
-
-#func set_path(path : Array) -> void:
-#	if get_parent() == null:
-#		yield(self, "ready")
-#	transform.origin = path[len(path) - 1]
-#	$Line.create_line(path, _points)
--- addons/waypoints/src/waypoints.gd
+++ addons/waypoints/src/waypoints.gd
@@ -17,6 +17,7 @@ func _init(pathing : MUP_Pathing, transformer, structure, waypoint_data_factory 
 
 
 func create_waypoint(pos : Vector2) -> void:
+	
 	var world_start = _resolve_position_from_id(-1)
 	var world_end = _transformer.transform(pos)
 	var path_points = _pathing.get_path(world_start, world_end)
@@ -26,6 +27,7 @@ func create_waypoint(pos : Vector2) -> void:
 
 
 func get_waypoint_id_from_pos(pos : Vector2):
+
 	var world_pos = _transformer.transform(pos)
 	for id in range(_waypoints.size()):
 		var waypoint_world_position = _waypoints[id].get_world_position()
@@ -44,24 +46,25 @@ func update_waypoints_from_pos(id : int, pos : Vector2) -> void:
 
 	_waypoints[id].set_world_position(world_end)
 	_waypoints[id].set_path(path_points)
-	_structure.set(id, _waypoints[id])
+	_structure.update(id, _waypoints[id])
 
 	
-	var position_next_waypoint = _resolve_position_from_id(next_id , true)
+	var position_next_waypoint = _resolve_position_from_id(next_id, true)
 	
 	if position_next_waypoint != null:
 		world_start = _transformer.transform(pos)
 		world_end = position_next_waypoint
 		path_points = _pathing.get_path(world_start, world_end)
 		_waypoints[next_id].set_path(path_points)
-		_structure.set(next_id, _waypoints[next_id])
+		_structure.update(next_id, _waypoints[next_id])
 
 
 func remove_waypoint(id : int) -> void:
+
 	var previous_id = id - 1
 	var next_id = id + 1
-	var start = _resolve_position_from_id(previous_id , true)
-	var end = _resolve_position_from_id(next_id , true)
+	var start = _resolve_position_from_id(previous_id, true)
+	var end = _resolve_position_from_id(next_id, true)
 		
 	if end != null:
 		var path_points = _pathing.get_path(start, end)
@@ -72,6 +75,7 @@ func remove_waypoint(id : int) -> void:
 
 
 func empty() -> bool:
+
 	return _waypoints.empty()
 
 
