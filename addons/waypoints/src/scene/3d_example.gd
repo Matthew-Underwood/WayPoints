4ac73441ff67a6f441143fe8505bab5f33316072
Matthew Underwood <farban6@gmail.com>
2024-04-04 13:24:49 +0100

    turn it into operations so we can have different operations for waypoints and paths

--- addons/waypoints/src/scene/3d_example.gd
+++ addons/waypoints/src/scene/3d_example.gd
@@ -4,41 +4,27 @@ var _waypoint_id
 var _world : MUW_World
 var _waypoints : MUW_Waypoints
 var _viewport : Viewport
+var _operations : MUW_Waypoints_Operations
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_viewport = get_viewport()
 	_world = MUW_World_Factory.new().create_3d(Vector3(0, -10, 0), self, _viewport.get_camera(), get_world())
 	_waypoints = _world.get_waypoints()
+	_operations = MUW_Waypoints_Operations.new(_world.get_waypoints(), _world)
 	
 	
 func _unhandled_input(event):
 
 	if Input.is_action_just_pressed("confirm_click"):
-		
-		var mouse_click = _viewport.get_mouse_position()
-		if !valid_click(mouse_click):
-			return
-		
-		_waypoint_id = _waypoints.get_waypoint_id_from_pos(mouse_click)
-		if _waypoint_id == null:
-
-			_waypoints.create_waypoint(mouse_click)
-			return
+		var click_pos = _viewport.get_mouse_position()
+		_operations.create(click_pos)
 			
-	if Input.is_action_pressed("confirm_click") && _waypoint_id != null:
-		var mouse_drag = _viewport.get_mouse_position()
-		var waypoint_id = _waypoints.get_waypoint_id_from_pos(mouse_drag)
-		if !valid_click(mouse_drag) || waypoint_id != null:
-			return
-		
-		_waypoints.update_waypoints_from_pos(_waypoint_id, mouse_drag)
 	
-	if Input.is_action_just_pressed("remove_waypoint") && _waypoint_id != null:
-		_waypoints.remove_waypoint(_waypoint_id)
-		_waypoint_id = null
-		
-		
-func valid_click(pos : Vector2) -> bool:
-	#TODO need to fix bug where you can create waypoint on player position. This needs handling somewhere else
-	return _world.is_walkable(pos)
+	if Input.is_action_pressed("confirm_click"):
+		var dragged_pos = _viewport.get_mouse_position()
+		_operations.update(dragged_pos)
+
+	
+	if Input.is_action_just_pressed("remove_waypoint"):
+		_operations.remove()
