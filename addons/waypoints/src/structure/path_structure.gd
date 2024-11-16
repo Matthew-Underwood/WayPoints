194e97ddcdd9d5d4f490c7df7a98b56be4cdce71
Matthew Underwood <farban6@gmail.com>
2024-11-16 20:22:21 +0000

    Use bezier lines for all path types

--- addons/waypoints/src/structure/path_structure.gd
+++ addons/waypoints/src/structure/path_structure.gd
@@ -25,108 +25,181 @@ func remove(id: int):
 
 
 func _set_path():
-	
-	_directions = {}
+    
+    if !_waypoint_data.empty():
+        for waypoint_data in _waypoint_data:
+            var points = waypoint_data.get_path()
+            for id in range(points.size()):
+                var relative_directions = {"previous" : Vector2(0, 0), "next" : Vector2(0, 0)}
+                var previous_id = id - 1
+                var next_id = id + 1
+                var current_position = _normalise_position(points[id]) 
 
-	if !_waypoint_data.empty():
-		for waypoint_data in _waypoint_data:
-			var points = waypoint_data.get_path()
-			for id in range(points.size()):
+                if previous_id >= 0:
+                    var previous_position = _normalise_position(points[previous_id])
+                    relative_directions["previous"] = _get_relative_position(previous_position, current_position)
 
-				var current_pos = points[id]
-				var current_pos_vec2 = Vector2(current_pos.x, current_pos.z)
-				if current_pos_vec2 - current_pos_vec2.floor() != Vector2(0.5, 0.5):
-					continue
-				
-				if id - 1 >= 0:
-					var previous_pos = points[id - 1]
-					var previous_direction = _get_direction(current_pos, previous_pos)
-					print(current_pos_vec2)
-					_set_direction(current_pos_vec2, previous_direction)
+                if next_id < points.size():
+                    var next_position = _normalise_position(points[next_id])
+                    relative_directions["next"] = _get_relative_position(next_position, current_position)
+                _set_direction(current_position, relative_directions)
+        
+        for pos in _directions:
+            var rgb_code = 0 
+            var direction_id = _directions[pos]["direction_id"]
+            rgb_code = direction_id
+            _map.update(pos, rgb_code)
+    _mask.set_shader_param("map", _map.get_map())
+   
 
-				if id + 1 < points.size():
-					var next_pos = points[id + 1]
-					var next_direction = _get_direction(current_pos, next_pos)
-					print(current_pos_vec2)
-					_set_direction(current_pos_vec2, next_direction)
-		
-		for pos in _directions:
-			var total = Vector3(0, 0, 0)
-			for direction in _directions[pos]["directions"]:
-				total.x += direction
-			for corner in _directions[pos]["corners"]:
-				total.y += corner
-			_map.update(pos, total)
-		_mask.set_shader_param("map", _map.get_map())
+func _normalise_position(pos : Vector3):
+
+    return Vector2(pos.x, pos.z).floor()
 
 
-func _set_direction(pos : Vector2, direction : int):
+func _set_direction(pos : Vector2, relative_positions : Dictionary):
 
-	pos = pos.floor()
-
-	if !_directions.has(pos):
-		_directions[pos] = {"directions" : {}, "corners" : {}}
-	
-	if !_directions[pos]["directions"].has(direction):
-		_directions[pos]["directions"][direction] = direction
-
-	print("Adding " + str(pos) + " with direction " + str(direction))
-
-	var corners = self._map_direction_to_corners(direction)
-	for corner in corners:
-		var offset = corners[corner]
-		self._set_corners(offset + pos, corner)
+    if !_directions.has(pos):
+        _directions[pos] = {"direction_id" : 0}
+    var direction_id = _get_direction_id(relative_positions)
+    _directions[pos]["direction_id"] += direction_id
 
 
-func _set_corners(pos : Vector2, corner : int):
+func _get_id_from_relative_pos(relative_directions : Dictionary):
 
-	if !_directions.has(pos):
-		_directions[pos] = {"directions" : {}, "corners" : {}}
+    var id = 0
+    match relative_directions:
+        #linear half line
+        {"next" : Vector2(1, 0)}:
+            id = 1
+        {"next" : Vector2(1, 1)}:
+            id = 2
+        {"next" : Vector2(0, 1)}:
+            id = 4
+        {"next" : Vector2(-1, 1)}:
+            id = 8
+        {"next" : Vector2(-1, 0)}:
+            id = 16
+        {"next" : Vector2(-1, -1)}:
+            id = 32
+        {"next" : Vector2(0, -1)}:
+            id = 64
+        {"next" : Vector2(1, -1)}:
+            id = 128
+        # linear full line
+        {"previous" : Vector2(-1, 0), "next" : Vector2(1, 0)}:
+            id = 1
+        {"previous" : Vector2(-1, -1), "next" : Vector2(1, 1)}:
+            id = 2
+        {"previous" : Vector2(0, -1), "next" : Vector2(0, 1)}:
+            id = 4
+        {"previous" : Vector2(1, -1), "next" : Vector2(-1, 1)}:
+            id = 8
+        {"previous" : Vector2(1, 0), "next" : Vector2(-1, 0)}:
+            id = 16
+        {"previous" : Vector2(1, 1), "next" : Vector2(-1, -1)}:
+            id = 32
+        {"previous" : Vector2(0, 1), "next" : Vector2(0, -1)}:
+            id = 64
+        {"previous" : Vector2(-1, 1), "next" : Vector2(1, -1)}:
+            id = 128
+        # bezier curve
+        {"previous" : Vector2(-1, 0), "next" : Vector2(1, 1)}:
+            id = 1
+        {"previous" : Vector2(0, -1), "next" : Vector2(-1, 1)}:
+            id = 2
+        {"previous" : Vector2(1, 0), "next" : Vector2(-1, -1)}:
+            id = 4
+        {"previous" : Vector2(0, 1), "next" : Vector2(1, -1)}:
+            id = 8
+        {"previous" : Vector2(-1, 0), "next" : Vector2(1, -1)}:
+            id = 16
+        {"previous" : Vector2(0, -1), "next" : Vector2(1, 1)}:
+            id = 32
+        {"previous" : Vector2(1, 0), "next" : Vector2(-1, 1)}:
+            id = 64
+        {"previous" : Vector2(0, 1), "next" : Vector2(-1, -1)}:
+            id = 128
+        # bezier curve
+        {"previous" : Vector2(0, 1), "next" : Vector2(1, 0)}:
+            id = 1
+        {"previous" : Vector2(-1, 0), "next" : Vector2(0, 1)}:
+            id = 2
+        {"previous" : Vector2(0, -1), "next" : Vector2(-1, 0)}:
+            id = 4
+        {"previous" : Vector2(1, 0), "next" : Vector2(0, -1)}:
+            id = 8
+        {"previous" : Vector2(0, -1), "next" : Vector2(1, 0)}:
+            id = 16
+        {"previous" : Vector2(1, 0), "next" : Vector2(0, 1)}:
+            id = 32
+        {"previous" : Vector2(0, 1), "next" : Vector2(-1, 0)}:
+            id = 64
+        {"previous" : Vector2(-1, 0), "next" : Vector2(0, -1)}:
+            id = 128
+        #bezier curve
+        {"previous" : Vector2(1, -1), "next" : Vector2(1, 1)}:
+            id = 1
+        {"previous" : Vector2(1, 1), "next" : Vector2(-1, 1)}:
+            id = 2
+        {"previous" : Vector2(-1, 1), "next" : Vector2(-1, -1)}:
+            id = 4
+        {"previous" : Vector2(-1, -1), "next" : Vector2(1, -1)}:
+            id = 8
+        {"previous" : Vector2(-1, -1), "next" : Vector2(1, -1)}:
+            id = 16
+    return id
+       
 
-	if !_directions[pos]["corners"].has(corner):
-		_directions[pos]["corners"][corner] = corner
+func _get_direction_id(relative_directions : Dictionary):
 
-	print("Adding " + str(pos) + " with corner " + str(corner))
+    var id = 0
 
+    match relative_directions:
+        {"previous" : Vector2(0, 0), "next" : Vector2(1, 0)}:
+            id = 1
+        {"previous" : Vector2(0, 0), "next" : Vector2(1, 1)}:
+            id = 2
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, 1)}:
+            id = 4
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, 1)}:
+            id = 8
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, 0)}:
+            id = 16
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, -1)}:
+            id = 32
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, -1)}:
+            id = 64
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, -1)}:
+            id = 128
+    return id
+       
 
-func _get_direction(pos : Vector3, pos2 : Vector3) -> int:
+func _get_direction_id(relative_directions : Dictionary):
 
-	# for x in range(-1, 2):
-	# 	for z in range(-1, 2):
-	# 		var direction = Vector3(x, 0, z)
-	# 		var angle = Vector3(0, 0, 1).signed_angle_to(direction, Vector3(0, 1, 0))
-	# 		var id = str(angle).sha1_text().left(4)
-	# 		print("Direction : " + str(direction))
-	# 		print("SHA1 : " + str(id))
+    var id = 0
 
+    match relative_directions:
+        {"previous" : Vector2(0, 0), "next" : Vector2(1, 0)}:
+            id = 1
+        {"previous" : Vector2(0, 0), "next" : Vector2(1, 1)}:
+            id = 2
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, 1)}:
+            id = 4
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, 1)}:
+            id = 8
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, 0)}:
+            id = 16
+        {"previous" : Vector2(0, 0), "next" : Vector2(-1, -1)}:
+            id = 32
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, -1)}:
+            id = 64
+        {"previous" : Vector2(0, 0), "next" : Vector2(0, -1)}:
+            id = 128
+    return id
+            
 
-	var direction = Vector3(pos.x - pos2.x, 0, pos.z - pos2.z)
-	var angle = Vector3(0, 0, -1).signed_angle_to(direction, Vector3(0, 1, 0))
-	var id = str(angle).sha1_text().left(4)
-	
-	var directions = {
-		"47e2" : 1, #NE
-		"6be4" : 2, #E
-		"dffa" : 4, #SE
-		"9912" : 8, #S
-		"b658" : 16, #SW
-		"4ffb" : 32, #W
-		"340d" : 64, #NW
-		"4786" : 128 #N
-	}
-	return directions[id]
+func _get_relative_position(pos1 : Vector2, pos2 : Vector2):
 
-
-func _map_direction_to_corners(direction : int) -> Dictionary:
-	
-	var offset = {}
-	match direction:
-		8:
-			offset = {2 : Vector2(0, 1), 1: Vector2(1, 0)}
-		128:
-			offset = {2 : Vector2(-1, 0), 1: Vector2(0, -1)}
-		2:
-			offset = {8 : Vector2(0, -1), 4: Vector2(1, 0)}
-		32:
-			offset = {8 : Vector2(-1, 0), 4: Vector2(0, 1)}
-	return offset
+    return  pos1 - pos2
+    
