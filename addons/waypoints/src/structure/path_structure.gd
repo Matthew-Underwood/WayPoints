f9e139cd555709f2c1ad712ca22f050108d21c11
Matthew Underwood <farban6@gmail.com>
2025-11-06 16:50:28 +0000

    Simplify the shader to only focus on paths. Path ids go up to 128 per tile rather than 256. Corners removed and will be reworked

--- addons/waypoints/src/structure/path_structure.gd
+++ addons/waypoints/src/structure/path_structure.gd
@@ -14,37 +14,14 @@ func send(path_store : MUW_Path_Directions_Store):
 
 	if !path_store.is_empty():
 		var directions = path_store.get_all_directions()
-		var corners = path_store.get_all_corners()
 
 		for pos in directions:
 			var path_total = 0
-			var corner_total = 0
 			var col = Vector3.ZERO
 
 			for id in directions[pos]:
 				path_total += directions[pos][id]
 
-			if corners.has(pos):
-				for id in corners[pos]:
-					corner_total += corners[pos][id]
-				corners.erase(pos)
-
-
-			if path_total == 256:
-				col = Vector3(0, 1, corner_total)
-			if path_total > 256:
-				col = Vector3(path_total - 256, 1, corner_total)
-			if path_total < 256:
-				col = Vector3(path_total, 0, corner_total)
-
-			_map.update(pos, col)
-
-		# Finish off with positions that only contain corners
-		for pos in corners:
-			var corner_total = 0
-			for id in corners[pos]:
-				corner_total += corners[pos][id]
-			var col = Vector3(0, 0, corner_total)
-			_map.update(pos, col)
+			_map.update(pos, Vector3(path_total, 0, 0))
 
 		_mask.set_shader_param("bezier_path_map", _map.get_map())
